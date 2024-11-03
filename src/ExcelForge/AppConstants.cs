3d523914d926463a47364dc9b2637dd6b5c533ba
Alex Mercer <alex.mercer@pm.me>
2024-11-04 08:44:39 +0900

    Add GitHub Releases auto-updater

--- src/ExcelForge/AppConstants.cs
+++ src/ExcelForge/AppConstants.cs
@@ -4,8 +4,8 @@ internal static class AppConstants
 {
 
     public const string AppName        = "ExcelForge";
-    public const string Version        = "0.3.5";
-    public const string VersionDisplay = "v0.3.5";
+    public const string Version        = "0.4.0";
+    public const string VersionDisplay = "v0.4.0";
 
     public const string GitHubOwner   = "BaadHackor";
     public const string GitHubRepo    = "excelforge";
